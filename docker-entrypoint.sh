cb3088535ecb7a53dd5ae765a9acc13723456cf5
Víctor González <victor.gonzalez@geomati.co>
2017-11-20 14:52:14 +0100

    Fix /var/geonode-overrides rsync

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 DEST=/var/geonode/geonode
-OVERRIDES=/var/geonode-overrides
+OVERWRITES=/var/geonode-overwrites
 CATALINA_SH=/var/lib/tomcat8/bin/catalina.sh
 
 service apache2 start
-$CATALINA_SH start
 
-if [ -d "$OVERRIDES" ]; then
+function update {
+  rsync -rlpt "$OVERWRITES"/* "$DEST/"
+  chown -R www-data: $DEST
+}
+
+if [ -d "$OVERWRITES" ]; then
   $CATALINA_SH start
+  update
   while true; do
-    inotifywait -mr "$OVERRIDES" | while read line; do
-      rsync -a "$OVERRIDES" "$DEST"
+    inotifywait -mr "$OVERWRITES" | while read line; do
+      update
     done
   done
 else
